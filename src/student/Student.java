dd08089d2311855af8bd01470d79f6e62a6a1161
raven18 <kaur9720@sheridancollege.ca>
2022-01-21 09:35:13 -0500

    first commit

---
+++ src/student/Student.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author raven
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+    Student1[] studentList = new Student1[3];
+    
+    studentList[0] = new Student1("s1", "Raven");
+    studentList[1] = new Student1("s2", "Anna");
+    studentList[2] = new Student1("s3", "Peter");
+    
+    for(int i =0; i<3; i++)
+    {
+        System.out.println(studentList[i]);
+       
+    }
+    
+}
+}
---
+++ src/student/Student1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author raven
+ */
+public class Student1 {
+    private String studentID;
+            private String studentName;
+
+    public Student1(String studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+            
+}
