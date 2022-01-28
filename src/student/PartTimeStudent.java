86d5285aab09a198f6e5f1e302c938d588b08d24
raven18 <kaur9720@sheridancollege.ca>
2022-01-28 10:27:01 -0500

    second commit : array size changed from 3 to 4 in student.java

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author raven
+ */
+public class PartTimeStudent extends Student1 {
+    private int numCourses ;
+
+    public PartTimeStudent(int numCourses, String studentID, String studentName) {
+        super(studentID, studentName);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
